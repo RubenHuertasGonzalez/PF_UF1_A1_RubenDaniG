5f409f32238d3fc570c7c19d820ea36b1d693219
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-11-20 12:16:43 +0100

    conexio al server amb sockets

--- app/src/main/java/com/ruben/pf_uf1_a1_rubendanig/MainActivity.kt
+++ app/src/main/java/com/ruben/pf_uf1_a1_rubendanig/MainActivity.kt
@@ -5,39 +5,57 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextCodi: EditText
+    private lateinit var enviarButton: Button
+    private lateinit var mSocket: Socket
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editTextCodi = findViewById<EditText>(R.id.editTextTextCodi)
-
-        val enviarButton = findViewById<Button>(R.id.buttonEnviarDades)
+        editTextCodi = findViewById(R.id.editTextTextCodi)
+        enviarButton = findViewById(R.id.buttonEnviarDades)
 
         SocketHandler.setSocket()
-
-        val mSocket = SocketHandler.getSocket()
+        mSocket = SocketHandler.getSocket()
 
         mSocket.connect()
 
         enviarButton.setOnClickListener {
-            val codigo = editTextCodi.text.toString()
-            val mensaje = "Codigo $codigo enviado"
-            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+            try {
+                val codigo = editTextCodi.text.toString()
+                val mensaje = "Código $codigo enviado"
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
 
-            mSocket.emit("counter")
+                mSocket.emit("incrementCounter")
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "Error al enviar el código", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        mSocket.on("counter"){args->
-            if (args[0]!=null){
-                val counter = args[0] as Int
+        mSocket.on("counter", onCounter)
+    }
 
-                runOnUiThread{
-                }
+    private val onCounter = Emitter.Listener { args ->
+        runOnUiThread {
+            if (args.isNotEmpty() && args[0] is Int) {
+                val counter = args[0] as Int
+                // Aquí puedes realizar la lógica para actualizar la UI con el contador
+                // Por ejemplo, puedes mostrar el contador en un TextView
+                // textViewCounter.text = counter.toString()
             }
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Desconectar el socket cuando la actividad se destruye
+        mSocket.disconnect()
+    }
 }
-
-
